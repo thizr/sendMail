7e4116a57e10da45aec609bc97d54b40c7f26282
thzr <thiernondiaye122@gmail.com>
2022-01-02 14:01:53 +0000

    attachment : multiple files

--- middlewares/validator.middleware.js
+++ middlewares/validator.middleware.js
@@ -3,13 +3,13 @@ const validator = require('../helpers/validate.helper')
 
 const input = (req, res, next) =>{
 
-    const validatiionRules = {
+    const validationRules = {
         "to" : "required|email",
         "subject" : "string",
         "text" : "required|string|mail",
     }
 
-    validator(req.body, validatiionRules, {}, (error, status)=>{
+    validator(req.body, validationRules, {}, (error, status)=>{
         if (!status) {
             return res.status(412).json({
                 success : false,
